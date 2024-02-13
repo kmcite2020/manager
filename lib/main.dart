af836260cc494fcf507d349968222a2335ce877c
kmcite2020 <lumia.college.learning.458@gmail.com>
2024-02-13 11:38:25 +0500

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,10 @@
-import 'package:colornames/colornames.dart';
 import 'package:flex_color_scheme/flex_color_scheme.dart';
 import 'package:flutter/material.dart';
 import 'package:manager/extensions.dart';
-import 'package:path_provider/path_provider.dart';
 
 import 'manager.dart';
-import 'navigator_injected.dart';
-import 'state_manager/management/complex.dart';
-import 'state_manager/management/manager.dart';
 
 final simpleCounterRM = RM.simple(() => 5);
-final materialColorRM = RM.complex(MaterialColorRM());
 final borderRadiusRM = RM.simple(() => 8.0);
 final useMaterial3 = RM.simple(() => true);
 final paddingRM = RM.simple(() => 8.0);
@@ -24,55 +18,32 @@ final periodicValuesRM = RM.stream(
   ),
 );
 
-final directoryRM = RM.future(getApplicationDocumentsDirectory);
-final themeModeRM = RM.manager(ThemeModeRM());
-
-class MaterialColorRM extends Complex<MaterialColor> {
-  MaterialColorRM() : super(Colors.amber);
-  void onChanged(MaterialColor? materialColor) => setState(materialColor!);
-}
-
-void main() {
-  runApp(const App());
-}
+void main() => runApp(const App());
 
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return themeModeRM.build(
-      (themeMode) => materialColorRM.build(
-        (materialColor) => MaterialApp(
-          navigatorKey: RM.navigatorKey,
-          debugShowCheckedModeBanner: false,
-          themeAnimationDuration: Duration(milliseconds: 700),
-          themeMode: themeMode,
-          theme: FlexThemeData.light(
-            colorScheme: ColorScheme.fromSwatch(
-              primarySwatch: materialColor,
-              brightness: Brightness.light,
-            ),
-            subThemesData: FlexSubThemesData(
-              defaultRadius: borderRadiusRM(),
-            ),
-            useMaterial3: useMaterial3(),
-            lightIsWhite: true,
-          ),
-          darkTheme: FlexThemeData.dark(
-            colorScheme: ColorScheme.fromSwatch(
-              primarySwatch: materialColor,
-              brightness: Brightness.dark,
-            ),
-            subThemesData: FlexSubThemesData(
-              defaultRadius: borderRadiusRM(),
-            ),
-            useMaterial3: useMaterial3(),
-            darkIsTrueBlack: true,
-          ),
-          home: HomePage(),
+    return MaterialApp(
+      navigatorKey: RM.navigatorKey,
+      debugShowCheckedModeBanner: false,
+      themeAnimationDuration: Duration(milliseconds: 700),
+      theme: FlexThemeData.light(
+        subThemesData: FlexSubThemesData(
+          defaultRadius: borderRadiusRM(),
         ),
+        useMaterial3: useMaterial3(),
+        lightIsWhite: true,
       ),
+      darkTheme: FlexThemeData.dark(
+        subThemesData: FlexSubThemesData(
+          defaultRadius: borderRadiusRM(),
+        ),
+        useMaterial3: useMaterial3(),
+        darkIsTrueBlack: true,
+      ),
+      home: HomePage(),
     );
   }
 }
@@ -90,57 +61,10 @@ class HomePage extends StatelessWidget {
       ),
       body: ListView(
         children: [
-          '${simpleCounterRM.value}'.text(textScaleFactor: 5).pad(),
-          periodicValuesRM.loading
-              ? CircularProgressIndicator().pad()
-              : periodicValuesRM().text(textScaleFactor: 5).pad(),
-          directoryRM.loading
-              ? CircularProgressIndicator().pad()
-              : directoryRM().text().pad(),
           ElevatedButton(
-            onPressed: () {
-              simpleCounterRM(
-                simpleCounterRM() + 1,
-              );
-            },
-            child: '+'.text(textScaleFactor: 2),
+            onPressed: () {},
+            child: 'child'.text(),
           ).pad(),
-          ElevatedButton(
-            onPressed: () {
-              simpleCounterRM(
-                simpleCounterRM() - 1,
-              );
-              navigator.to(HomePage());
-            },
-            child: '-'.text(textScaleFactor: 2),
-          ).pad(),
-          DropdownButtonFormField(
-            value: themeModeRM(),
-            items: ThemeMode.values
-                .map(
-                  (eachThemeMode) => DropdownMenuItem(
-                    child: eachThemeMode.name.toUpperCase().text(),
-                    value: eachThemeMode,
-                  ),
-                )
-                .toList(),
-            onChanged: (_) {
-              themeModeRM.add(ThemeModeEvent(_!));
-            },
-          ).pad(),
-          DropdownButtonFormField(
-            value: materialColorRM.state,
-            items: Colors.primaries
-                .map(
-                  (eachThemeMode) => DropdownMenuItem(
-                    child: eachThemeMode.colorName.toUpperCase().text(),
-                    value: eachThemeMode,
-                  ),
-                )
-                .toList(),
-            onChanged: materialColorRM.onChanged,
-          ).pad(),
-          'Padding'.text().center().pad(),
           Slider(
             label: 'Padding',
             min: 5,
@@ -166,15 +90,4 @@ class HomePage extends StatelessWidget {
       ),
     );
   }
-}
-
-class ThemeModeRM extends Manager<ThemeModeEvent, ThemeMode> {
-  ThemeModeRM() : super(ThemeMode.system) {
-    on<ThemeModeEvent>((event, emit) => emit(event.themeMode));
-  }
-}
-
-class ThemeModeEvent {
-  final ThemeMode themeMode;
-  ThemeModeEvent(this.themeMode);
 }
--- lib/state_manager/management/manager.dart
+++ lib/state_manager/management/manager.dart
@@ -7,7 +7,6 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:manager/state_manager/ui/widgets.dart';
-
 import 'simple.dart';
 
 /// Event State Management -> Just like Bloc package
@@ -16,44 +15,27 @@ class Manager<E, T> extends Simple<T> {
   Manager(this.initialBase) : super(() => initialBase);
   final handlers = <Handler>[];
   void on<E>(
-    HandlerFunction<E, T> handlerFunction,
+    EventRegistrar<E, T> handlerFunction,
   ) {
-    final registered = handlers.any((handler) => handler.type == E);
+    final registered = handlers.any((handler) => handler.$2 == E);
     assert(
       !registered,
       'on<$E> was called multiple times.',
     );
     handlers.add(
-      Handler(
-        isType: (e) => e is E,
-        type: E,
-        function: handlerFunction,
+      (
+        (e) => e is E,
+        E,
+        handlerFunction,
       ),
     );
     print('${E} registered');
   }
 
   void add(E event) {
-    final index = handlers.indexWhere((e) => e.isType(event));
-    final eventType = event.runtimeType;
-    assert(
-      index != -1,
-      'on<$eventType>(...) must be called before add($eventType)',
-    );
-
-    final fn = handlers[index].function;
-    final emitter = Emitter<T>(
-      (newState) => state = newState,
-    );
-
-    final result = fn(event, emitter) as FutureOr<void>;
-    if (result is Future) {
-      result.then(
-        (_) => emitter._disable(),
-      );
-    } else {
-      emitter._disable();
-    }
+    final index = handlers.indexWhere((e) => e.$1(event));
+    handlers[index].$3(event, ((newState) => state = newState))
+        as FutureOr<void>;
   }
 
   @visibleForTesting
@@ -64,48 +46,13 @@ class Manager<E, T> extends Simple<T> {
       );
 }
 
-typedef HandlerFunction<E, State> = FutureOr<void> Function(
-  E event,
-  Emitter<State> emit,
+typedef Emitter<T> = void Function(T newState);
+typedef Handler = (
+  bool Function(dynamic value) isType,
+  Type type,
+  Function function,
 );
-
-class Handler {
-  const Handler({
-    required this.isType,
-    required this.type,
-    required this.function,
-  });
-  final bool Function(dynamic value) isType;
-  final Type type;
-  final Function function;
-}
-
-class Emitter<S> {
-  Emitter(this._updater);
-  final void Function(S newState) _updater;
-
-  bool _enabled = true;
-
-  void call(S newState) {
-    assert(
-      _enabled,
-      '''\n\n
-emit was called after an event handler completed normally.
-This is usually due to an unawaited future in an event handler.
-  **BAD**
-  on<Event>((event, emit) {
-    future.whenComplete(() => emit(...));
-  });
-  **GOOD**
-  on<Event>((event, emit) async {
-    await future.whenComplete(() => emit(...));
-  });
-''',
-    );
-    _updater(newState);
-  }
-
-  void _disable() {
-    _enabled = false;
-  }
-}
+typedef EventRegistrar<Event, State> = FutureOr<void> Function(
+  Event event,
+  Emitter<State> setState,
+);
