24619f6c2da73c3435139b5224f251deb3196fbf
kmcite2020 <lumia.college.learning.458@gmail.com>
2024-06-27 01:27:34 +0500

    updated

--- lib/extensions.dart
+++ lib/extensions.dart
@@ -1,20 +1,52 @@
 part of 'manager.dart';
 
 extension DynamicExtensions on dynamic {
-  Widget text({double textScaleFactor = 1}) => Text(toString());
+  Text text({double textScaleFactor = 1}) {
+    return Text(
+      toString(),
+      textScaler: TextScaler.linear(textScaleFactor),
+    );
+  }
 }
 
 extension WidgetExtensions on Widget {
-  Widget pad({EdgeInsets custom = const EdgeInsets.all(8)}) {
+  Padding pad({
+    double? all,
+    double? right,
+    double? left,
+    double? top,
+    double? bottom,
+    double? horizontal,
+    double? vertical,
+  }) {
+    EdgeInsetsGeometry edgeInsets = EdgeInsets.zero;
+
+    if (all != null) {
+      edgeInsets = EdgeInsets.all(all);
+    } else if (horizontal != null || vertical != null) {
+      edgeInsets = EdgeInsets.symmetric(
+        vertical: vertical ?? 0.0,
+        horizontal: horizontal ?? 0.0,
+      );
+    } else if (right != null || left != null || top != null || bottom != null) {
+      edgeInsets = EdgeInsets.only(
+        left: left ?? 0.0,
+        right: right ?? 0.0,
+        top: top ?? 0.0,
+        bottom: bottom ?? 0.0,
+      );
+    } else {
+      edgeInsets = EdgeInsets.all(8.0);
+    }
+
     return Padding(
-      padding: EdgeInsets.all(8),
+      padding: edgeInsets,
       child: this,
     );
   }
 
-  Widget center() {
-    return Center(
-      child: this,
-    );
-  }
+  Widget center() => Center(child: this);
+  Card card() => Card(child: this);
 }
+
+String get randomID => Uuid().v8();
--- lib/manager.dart
+++ lib/manager.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 import 'dart:collection';
 import 'package:stream_transform/stream_transform.dart';
 import 'package:flutter/material.dart';
+import 'package:uuid/uuid.dart';
 
 export 'package:manager/manager.dart';
 export 'package:manager/hydrated.dart';
